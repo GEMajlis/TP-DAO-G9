4cc6a0dfc5d6e86fb95fe9bc34c453ae895884f5
marimilhas <marimilhas@gmail.com>
2025-11-01 17:37:10 -0300

    Base del inicio, menu y footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { Menu } from "./components/Menu.jsx"; // <-- Corrección de extensión
+import { Footer } from "./components/Footer.jsx"; // <-- Corrección de extensión
+import { Inicio } from "./components/Inicio.jsx"; // <-- Corrección de extensión
 
 function App() {
   return (
-    <h1>Bienvenidos a Desarrollo de Software!</h1>
+    <>
+      <BrowserRouter>
+        <Menu />
+        <div className="divBody">
+          <Routes>
+            <Route path="/inicio" element={<Inicio />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
-
 export default App;
