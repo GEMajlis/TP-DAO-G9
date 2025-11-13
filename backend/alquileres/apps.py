c63ca5c6c1ec8b0c82ef79ad66048142c275e636
GEMajlis <gonzalo.ezequiel.majlis@gmail.com>
2025-11-13 19:37:32 -0300

    fix: correccion al abmc de clientes

--- backend/alquileres/apps.py
+++ backend/alquileres/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AlquileresConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'alquileres'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "alquileres"
--- backend/clientes/forms.py
+++ backend/clientes/forms.py
@@ -2,13 +2,22 @@ from django.forms import ModelForm
 from .models import Cliente
 from django import forms
 
+
 class ClienteForm(ModelForm):
     class Meta:
         model = Cliente
-        fields = ['dni', 'nombre', 'apellido', 'telefono']
+        fields = ["dni", "nombre", "apellido", "telefono"]
         widgets = {
-            'dni': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'DNI'}),
-            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre'}),
-            'apellido': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido'}),
-            'telefono': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Teléfono'}),
+            "dni": forms.NumberInput(
+                attrs={"class": "form-control", "placeholder": "DNI"}
+            ),
+            "nombre": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Nombre"}
+            ),
+            "apellido": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Apellido"}
+            ),
+            "telefono": forms.NumberInput(
+                attrs={"class": "form-control", "placeholder": "Teléfono"}
+            ),
         }
